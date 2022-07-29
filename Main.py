171f7a6b6a0e3a85970d35aae6abdcdb0b96acd8
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-07-29 15:21:39 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
+from VRP_Model import *
+from Solver import *
 
+#1st question
+m1 = Model()
+m1.BuildModel()
+
+#3nd question
+s1 = Solver(m1)
+sol = s1.solve()
+
+#2nd question
+dist = s1.CalculateDistance(s1.sol)
+print("Total distance: ", dist)
+
+#4th question
+s1.LocalSearch()
+s1.ReportSolution(s1.sol)
+print("Total routes (after local search): ", len(s1.sol.routes))
+
+#5th question
+s1.VND()
+s1.ReportSolution(s1.sol)
