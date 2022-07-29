9ea857ea36d0fd9a06f5656598e2205b4ebc03c2
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-07-29 16:48:05 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,19 +5,30 @@ from Solver import *
 m1 = Model()
 m1.BuildModel()
 
-#3nd question
+#3rd question
+print()
+print("Solution with Nearest Neighbor Method:")
+print()
 s1 = Solver(m1)
 sol = s1.solve()
+print()
 
 #2nd question
 dist = s1.CalculateDistance(s1.sol)
-print("Total distance: ", dist)
+print("Total distance with Nearest Neighbor Method: ", dist)
+print()
 
 #4th question
+print("Solution after Local Search:")
+print()
 s1.LocalSearch()
 s1.ReportSolution(s1.sol)
-print("Total routes (after local search): ", len(s1.sol.routes))
+print()
+print("Total routes after Local Search: ", len(s1.sol.routes))
+print()
 
 #5th question
+print("Solution with VND Algorithm:")
+print()
 s1.VND()
 s1.ReportSolution(s1.sol)
